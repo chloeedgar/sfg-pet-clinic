c22f21b956bfd6b906662da309c5bd0a50a68243
Chloe Edgar <chloeedgar1996@hotmail.co.uk>
2021-11-19 15:34:20 +0000

    Added owners list and updated controllers. Closes #11

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -33,7 +33,7 @@ public class DataLoader implements CommandLineRunner {
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenne");
 
-        ownerService.save(owner1);
+        ownerService.save(owner2);
 
         System.out.println("Loaded Owners...");
 
