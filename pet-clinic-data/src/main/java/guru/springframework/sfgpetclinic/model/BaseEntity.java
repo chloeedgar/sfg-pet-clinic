7edd59853a2e362c178116845316f1c86345c3e6
Chloe Edgar <chloeedgar1996@hotmail.co.uk>
2021-11-26 15:15:52 +0000

    updated owner with JPA entity mappings. Closes #40

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -5,6 +5,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
+
 @MappedSuperclass
 public class BaseEntity implements Serializable {
     @Id
